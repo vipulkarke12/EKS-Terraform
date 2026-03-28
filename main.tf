d0682ac2850001b742fdadcbbbd32558bc9d8cbb
vipul karke <vipulkarke.vk@gmail.com>
2026-03-28 19:54:40 -0400

    Update scaling configuration for desired instance size

--- main.tf
+++ main.tf
@@ -115,8 +115,8 @@ resource "aws_eks_node_group" "vipul" {
 
   scaling_config {
     desired_size = 2
-    max_size     = 1
-    min_size     = 1
+    max_size     = 2
+    min_size     = 2
   }
 
   instance_types = ["c7i-flex.large"]
