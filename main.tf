b9301ddfe1038dd1a7c3b51f5176ddb9c910daa8
vipul karke <vipulkarke.vk@gmail.com>
2026-03-28 20:22:23 -0400

    Rename VPC and related resources from 'vipul' to 'vipulk'

--- main.tf
+++ main.tf
@@ -2,55 +2,55 @@ provider "aws" {
   region = "ca-central-1"
 }
 
-resource "aws_vpc" "vipul_vpc" {
+resource "aws_vpc" "vipulk_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "vipul-vpc"
+    Name = "vipulk-vpc"
   }
 }
 
-resource "aws_subnet" "vipul_subnet" {
+resource "aws_subnet" "vipulk_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.vipul_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.vipul_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.vipulk_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.vipulk_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["ca-central-1a", "ca-central-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "vipul-subnet-${count.index}"
+    Name = "vipulk-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "vipul_igw" {
-  vpc_id = aws_vpc.vipul_vpc.id
+resource "aws_internet_gateway" "vipulk_igw" {
+  vpc_id = aws_vpc.vipulk_vpc.id
 
   tags = {
-    Name = "vipul-igw"
+    Name = "vipulk-igw"
   }
 }
 
-resource "aws_route_table" "vipul_route_table" {
-  vpc_id = aws_vpc.vipul_vpc.id
+resource "aws_route_table" "vipulk_route_table" {
+  vpc_id = aws_vpc.vipulk_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.vipul_igw.id
+    gateway_id = aws_internet_gateway.vipulk_igw.id
   }
 
   tags = {
-    Name = "vipul-route-table"
+    Name = "vipulk-route-table"
   }
 }
 
 resource "aws_route_table_association" "a" {
   count          = 2
-  subnet_id      = aws_subnet.vipul_subnet[count.index].id
-  route_table_id = aws_route_table.vipul_route_table.id
+  subnet_id      = aws_subnet.vipulk_subnet[count.index].id
+  route_table_id = aws_route_table.vipulk_route_table.id
 }
 
-resource "aws_security_group" "vipul_cluster_sg" {
-  vpc_id = aws_vpc.vipul_vpc.id
+resource "aws_security_group" "vipulk_cluster_sg" {
+  vpc_id = aws_vpc.vipulk_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +60,12 @@ resource "aws_security_group" "vipul_cluster_sg" {
   }
 
   tags = {
-    Name = "vipul-cluster-sg"
+    Name = "vipulk-cluster-sg"
   }
 }
 
-resource "aws_security_group" "vipul_node_sg" {
-  vpc_id = aws_vpc.vipul_vpc.id
+resource "aws_security_group" "vipulk_node_sg" {
+  vpc_id = aws_vpc.vipulk_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,36 +82,36 @@ resource "aws_security_group" "vipul_node_sg" {
   }
 
   tags = {
-    Name = "vipul-node-sg"
+    Name = "vipulk-node-sg"
   }
 }
 
 # 1. Add missing VPC policy for cluster role
-resource "aws_iam_role_policy_attachment" "vipul_cluster_vpc_policy" {
-  role       = aws_iam_role.vipul_cluster_role.name
+resource "aws_iam_role_policy_attachment" "vipulk_cluster_vpc_policy" {
+  role       = aws_iam_role.vipulk_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
 }
 
-resource "aws_eks_cluster" "vipul" {
-  name     = "vipul-cluster"
-  role_arn = aws_iam_role.vipul_cluster_role.arn
+resource "aws_eks_cluster" "vipulk" {
+  name     = "vipulk-cluster"
+  role_arn = aws_iam_role.vipulk_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.vipul_subnet[*].id
-    security_group_ids = [aws_security_group.vipul_cluster_sg.id]
+    subnet_ids         = aws_subnet.vipulk_subnet[*].id
+    security_group_ids = [aws_security_group.vipulk_cluster_sg.id]
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.vipul_cluster_role_policy,
-    aws_iam_role_policy_attachment.vipul_cluster_vpc_policy,
+    aws_iam_role_policy_attachment.vipulk_cluster_role_policy,
+    aws_iam_role_policy_attachment.vipulk_cluster_vpc_policy,
   ]
 }
 
-resource "aws_eks_node_group" "vipul" {
-  cluster_name    = aws_eks_cluster.vipul.name
-  node_group_name = "vipul-node-group"
-  node_role_arn   = aws_iam_role.vipul_node_group_role.arn
-  subnet_ids      = aws_subnet.vipul_subnet[*].id
+resource "aws_eks_node_group" "vipulk" {
+  cluster_name    = aws_eks_cluster.vipulk.name
+  node_group_name = "vipulk-node-group"
+  node_role_arn   = aws_iam_role.vipulk_node_group_role.arn
+  subnet_ids      = aws_subnet.vipulk_subnet[*].id
 
   scaling_config {
     desired_size = 2
@@ -123,12 +123,12 @@ resource "aws_eks_node_group" "vipul" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.vipul_node_sg.id]
+    source_security_group_ids = [aws_security_group.vipulk_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "vipul_cluster_role" {
-  name = "vipul-cluster-role"
+resource "aws_iam_role" "vipulk_cluster_role" {
+  name = "vipulk-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -146,13 +146,13 @@ resource "aws_iam_role" "vipul_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "vipul_cluster_role_policy" {
-  role       = aws_iam_role.vipul_cluster_role.name
+resource "aws_iam_role_policy_attachment" "vipulk_cluster_role_policy" {
+  role       = aws_iam_role.vipulk_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "vipul_node_group_role" {
-  name = "vipul-node-group-role"
+resource "aws_iam_role" "vipulk_node_group_role" {
+  name = "vipulk-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -170,17 +170,17 @@ resource "aws_iam_role" "vipul_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "vipul_node_group_role_policy" {
-  role       = aws_iam_role.vipul_node_group_role.name
+resource "aws_iam_role_policy_attachment" "vipulk_node_group_role_policy" {
+  role       = aws_iam_role.vipulk_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "vipul_node_group_cni_policy" {
-  role       = aws_iam_role.vipul_node_group_role.name
+resource "aws_iam_role_policy_attachment" "vipulk_node_group_cni_policy" {
+  role       = aws_iam_role.vipulk_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "vipul_node_group_registry_policy" {
-  role       = aws_iam_role.vipul_node_group_role.name
+resource "aws_iam_role_policy_attachment" "vipulk_node_group_registry_policy" {
+  role       = aws_iam_role.vipulk_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
