d38be3e65beafafe1fa3a5dbe7417d39a3d8e14c
vipul karke <vipulkarke.vk@gmail.com>
2026-03-28 20:22:35 -0400

    Update output values to use 'vipulk' instead of 'vipul'

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.vipul.id
+  value = aws_eks_cluster.vipulk.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.vipul.id
+  value = aws_eks_node_group.vipulk.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.vipul_vpc.id
+  value = aws_vpc.vipulk_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.vipul_subnet[*].id
+  value = aws_subnet.vipulk_subnet[*].id
 }
